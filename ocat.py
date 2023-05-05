02aab77eb43296dd1439b67f47a5c412d159a5d3
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2023-05-05 11:20:49 -0400

    cleanup

--- ocat.py
+++ ocat.py
@@ -1,9 +1,7 @@
 import sys, os
-sys.path.append('/data/mta4/CUS/www/Usint/Ocat/ocat')
-sys.path.append('/data/mta4/CUS/www/Usint/Ocat/ocat/app')
-sys.path.append('/proj/sot/ska3/flight/lib/python3.8/site-packages')
-sys.path.append('/soft/SYBASE16.0/OCS-16_0/python/python34_64r/lib')
-os.environ.setdefault("SYBASE", "/soft/SYBASE16.0")
+#For finding the Application scripts in directory where the ocat.py script is located
+sys.path.insert(0,f"{os.path.dirname(os.path.realpath(__file__))}")
+sys.path.insert(1,f"{os.path.dirname(os.path.realpath(__file__))}/app")
 
 from app            import create_app, db       #--- in app/__init__.py
 from app.models     import User
