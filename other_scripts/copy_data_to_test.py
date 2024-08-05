21c6b8f510bf8931c812cc2abb808e5bf58c2ec1
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2024-08-05 13:10:05 -0400

    impreovements

--- other_scripts/copy_data_to_test.py
+++ other_scripts/copy_data_to_test.py
@@ -1,49 +1,45 @@
-#!/usr/bin/env /data/mta/Script/Python3.8/envs/ska3-shiny/bin/python
+#!/proj/sot/ska3/flight/bin/python
 
 #########################################################################################
 #                                                                                       #
 #       copy_data_to_test.py: copying the data to the test directory                    #
 #                                                                                       #
-#           author: t. isobe (tisobe@cfa.harvard.edu)                                   #
+#           author: w. aaron (william.aaron@cfa.harvard.edu)                            #
 #                                                                                       #
-#           last update: Aug 19, 2021                                                   #
+#           last update: Aug 05, 2024                                                   #
 #                                                                                       #
 #########################################################################################
 
-import sys
 import os
-import string
 import re
 import numpy
 
-o_dir = '/data/mta4/CUS/www/Usint/ocat/'
+OCAT_DIR = "/data/mta4/CUS/www/Usint/ocat"
+COPY_DIR = "/proj/web-cxc/cgi-gen/mta/Obscat/ocat"
 #
 #--- read the current test data list
 #
-with open('./updates_table.list', 'r') as f:
+with open(f'{COPY_DIR}/updates_table.list', 'r') as f:
     data1 = [line.strip() for line in f.readlines()]
 #
 #--- copy the real data list and approved list
 #
-cmd   = 'cp -f ' + o_dir + 'updates_table.list .'
-os.system(cmd)
+os.system(f"cp -f {OCAT_DIR}/updates_table.list {COPY_DIR}/updates_table.list")
 
-cmd   = 'cp -f ' + o_dir + 'approved .'
-os.system(cmd)
+os.system(f"cp -f {OCAT_DIR}/approved {COPY_DIR}/approved")
 #
 #--- read the updated test data list
 #
-with open('./updates_table.list', 'r') as f:
+with open(f'{COPY_DIR}/updates_table.list', 'r') as f:
     data2 = [line.strip() for line in f.readlines()]
 #
-#--- read the  list from the previous day
+#--- read the list from the previous day
 #
-if os.path.isfile('./comp_list'):
-    with open('./comp_list', 'r') as f:
+if os.path.isfile(f'{COPY_DIR}/comp_list'):
+    with open(f'{COPY_DIR}/comp_list', 'r') as f:
         data3 = [line.strip() for line in f.readlines()]
 else:
-    cmd = 'cp ./updates_table.list ./comp_list'
-    os.system(cmd)
+    os.system(f"cp {COPY_DIR}/updates_table.list {COPY_DIR}/comp_list")
     exit(1)
 #
 #--- compare lists and remove the test data added a day before
@@ -53,20 +49,25 @@ added = numpy.setdiff1d(data1, data3)
 if len(added) > 0:
     for ent in added:
         atemp = re.split('\t+', ent)
-        cmd   = 'rm -f ./updates/' + str(atemp[0]) 
-        os.system(cmd)
+        os.system(f"rm -f {COPY_DIR}/updates/{atemp[0]}")
 #
-#--- compare lists  add the data added in the real database
+#--- compare lists and add the data added in the real database
 #
 added = numpy.setdiff1d(data2, data3)
 
 if len(added) > 0:
     for ent in added:
         atemp = re.split('\t+', ent)
-        cmd   = 'cp -fp ' + o_dir + 'updates/' + str(atemp[0]) + ' ./updates/.'
-        os.system(cmd)
+        os.system(f"cp -fp {OCAT_DIR}/updates/{atemp[0]} {COPY_DIR}/updates/")
 
-    cmd = 'cp -r ./updates_table.list ./comp_list'
-    os.system(cmd)
+    os.system(f"cp -f {COPY_DIR}/updates_table.list {COPY_DIR}/comp_list")
 
+#
+#--- Copy Current TOO Schedule
+#
+os.system(f'cp -f {OCAT_DIR}/Info_save/too_contact_info/schedule  {COPY_DIR}/Info_save/too_contact_info/schedule')
 
+#
+#--- Copy cdo_warning_list
+#
+os.system(f"cp -f {OCAT_DIR}/cdo_warning_list {COPY_DIR}/cdo_warning_list")
